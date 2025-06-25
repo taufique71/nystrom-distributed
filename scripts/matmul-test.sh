849ec2dccc0d48a9e2fea2778dada97da2634e60
Taufique Hussain <mth@indiana.edu>
2025-06-25 14:17:22 -0700

    square matmul hybrid-parallelism experiment result updated

--- scripts/matmul-test.sh
+++ scripts/matmul-test.sh
@@ -4,7 +4,7 @@
 #SBATCH -C cpu
 #SBATCH -A m4293 # Sparsitute project (A Mathematical Institute for Sparse Computations in Science and Engineering)
 
-#SBATCH -t 0:05:00
+#SBATCH -t 0:10:00
 
 #SBATCH -N 8
 #SBATCH -J matmul
@@ -13,32 +13,33 @@
 module swap PrgEnv-gnu PrgEnv-intel
 module load python
 
-#ALG=matmul1gen
-#IMPL=python
-
 SYSTEM=perlmutter_cpu
 CORE_PER_NODE=128 # Never change. Specific to the system
 PER_NODE_MEMORY=256 # Never change. Specific to the system
-N_NODE=2
+N_NODE=4
 PROC_PER_NODE=8
 N_PROC=$(( $N_NODE * $PROC_PER_NODE ))
-THREAD_PER_PROC=$(( $CORE_PER_NODE / $PROC_PER_NODE ))
+CORE_PER_PROC=$(( $CORE_PER_NODE / $PROC_PER_NODE )) 
+THREAD_PER_PROC=$(( $CORE_PER_PROC * 2 )) # Set number of threads to be twice the number of physical cores ( using logical cores )
 PER_PROC_MEM=$(( $PER_NODE_MEMORY / $PROC_PER_NODE - 2)) #2GB margin of error
 export OMP_NUM_THREADS=$THREAD_PER_PROC
 export MKL_NUM_THREADS=$THREAD_PER_PROC
+#export OMP_PLACES=threads
+#export OMP_PROC_BIND=spread
 
 P1=1
 P2=1
 P3=1
 N1=50000
 N2=50000
-N3=50000
+N3=5000
 
 #for ALG in [ "matmul", "matmul1gen", "matmul1comm" ]; do
-for ALG in matmul
-#for ALG in matmul1gen matmul1comm 
+#for ALG in matmul
+for ALG in matmul1gen matmul1comm 
 do
     for IMPL in cpp python
+    #for IMPL in cpp
     do
         echo $ALG, $IMPL
         if [ "$ALG" == "matmul" ]; then
@@ -92,10 +93,12 @@ do
 
         if [ "$IMPL" == "cpp" ]; then
             srun -N $N_NODE -n $N_PROC -c $THREAD_PER_PROC --ntasks-per-node=$PROC_PER_NODE --cpu-bind=cores \
-                $BIN -p1 $P1 -p2 $P2 -p3 $P3 -n1 $N1 -n2 $N2 -n3 $N3 -alg $ALG >> $STDOUT_FILE
+                $BIN -p1 $P1 -p2 $P2 -p3 $P3 -n1 $N1 -n2 $N2 -n3 $N3 -alg $ALG &> $STDOUT_FILE
+            #srun -N $N_NODE -n $N_PROC -c $THREAD_PER_PROC --ntasks-per-node=$PROC_PER_NODE --cpu-bind=cores \
+                #check-hybrid.gnu.pm | sort -k4,4n -k6,6n &> blah.txt
         elif [ "$IMPL" == "python" ]; then
             srun -N $N_NODE -n $N_PROC -c $THREAD_PER_PROC --ntasks-per-node=$PROC_PER_NODE --cpu-bind=cores \
-                python $PY -p1 $P1 -p2 $P2 -p3 $P3 -n1 $N1 -n2 $N2 -n3 $N3 -alg $ALG >> $STDOUT_FILE
+                python $PY -p1 $P1 -p2 $P2 -p3 $P3 -n1 $N1 -n2 $N2 -n3 $N3 -alg $ALG &> $STDOUT_FILE
         fi
     done
 done
