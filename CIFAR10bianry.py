05cea020caadc397c859337b265be43cb51958f3
Mohammad Marufur Rahman <inkmarufnayem@gmail.com>
2025-09-13 23:18:54 -0400

    modified cifar10binary.py

--- CIFAR10bianry.py
+++ CIFAR10bianry.py
@@ -3,22 +3,21 @@ import argparse
 from torchvision.datasets import CIFAR10
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-n", "--nuberOfSamples", type=int, help="Number of rows and columns of the matrix", default=100)
+parser.add_argument("-path", "--path", type=str, help="Path to save binary file", default="./data")
 
 args = parser.parse_args()
-nuberOfSamples = args.nuberOfSamples
 
-print(f"Generating CIFAR10 Binary at ./data/cifar10.bin")
 # Download dataset (train + test)
-rawData = CIFAR10(root="./data", train=True, download=True, transform=None)
+rawData = CIFAR10(root=args.path, train=True, download=True, transform=None)
 allSamples = [np.array(img) for img, _ in rawData] 
-samples = np.stack(allSamples[:nuberOfSamples], axis=0)
-samples = samples.reshape(nuberOfSamples, -1)  # (n, 3072)
+samples = np.stack(allSamples, axis=0)
+samples = samples.reshape(-1, 3072)  # (n, 3072)
+print(f"Shape of Input matrix: {samples.shape}")
 
 samples = samples @ samples.T
 
 data = samples.flatten().astype(np.float64)
 
-with open("./data/cifar10.bin", "wb") as f:
+with open(args.path+"/cifar10.bin", "wb") as f:
     # then write matrix values
     data.tofile(f)
