2fa18e0085d1c025db009512cf8a676ae14bca6e
Taufique Hussain <mth@indiana.edu>
2025-09-16 19:13:23 -0700

    Scripts and sanitized csv from Nystrom 1d-1d and 2d-1d experiments added

--- scripts/parse-nystrom-experiment.py
+++ scripts/parse-nystrom-experiment.py
@@ -4,57 +4,73 @@ import pandas as pd
 
 def parse_experiment_file(file_path):
     filename = os.path.basename(file_path)
-
-    parts = filename.split('_')
+    
+    # nystrom-1d-noredist-1d_cpp_perlmutter-gpu_32_128_32_50000_5000_128x1x1_128x1x1.10
+    parts = re.split(r"[_.x]+", filename)
+    print(parts)
+    # parts = filename.split('_.')
     alg = parts[0]
     impl = parts[1]
-    nnode = int(parts[2])
-    nproc = int(parts[3])
+    system = parts[2]
+    nnode = int(parts[3])
+    nproc = int(parts[4])
+    nthread = int(parts[5])
+    n = int(parts[6])
+    r = int(parts[7])
+    matmul1p1 = int(parts[8])
+    matmul1p2 = int(parts[9])
+    matmul1p3 = int(parts[10])
+    matmul2p1 = int(parts[11])
+    matmul2p2 = int(parts[12])
+    matmul2p3 = int(parts[13])
 
     with open(file_path, 'r') as file:    
         content = file.read()
     
     # Extract matrix dimensions (assuming they are in the format "MxN")
     dimension_match = re.search(r'(\d+)x(\d+)', content)
-    # pattern = r'(\w+)\s+(\d+)x(\d+)\s+with\s+(\d+)x(\d+)\s+on\s+(\d+)x(\d+)x(\d+)\s+grid'
-    # pattern = r"(\d+)x(\d+) matrix to rank (\d+) on (\d+)x(\d+)x(\d+) grid" 
     pattern = r"Nystrom approximation of (\d+)x(\d+) matrix to rank (\d+) using ([\w-]+)"
     match = re.search(pattern, content)
     if match:
-        n = int(match.group(1))  # Rows of matrix A
-        m = int(match.group(2))  # Columns of matrix A
-        r = int(match.group(3))  # Target rank of Nystrom
+        assert n == int(match.group(1)) # Rows of matrix A
+        assert n == int(match.group(2)) # Columns of matrix A
+        assert r == int(match.group(3)) # Target rank of Nystrom
     else:
         raise ValueError("Line format is incorrect.")
 
     # Extract timing information
-    matmul1_grid = re.search(r'matmul1 in (\d+)x(\d+)x(\d+) grid', content)
     gen_omega_time = re.search(r'Time to generate Omega:\s*([\d.]+) sec', content)
-    first_dgemm_time = re.search(r'Time for first dgemm:\s*([\d.]+) sec', content)
-    matmul2_grid = re.search(r'matmul2 in (\d+)x(\d+)x(\d+) grid', content)
-    redistrib_y_time = re.search(r'Time to redistribute Y:\s*([\d.]+) sec', content)
-    second_dgemm_time = re.search(r'Time for second dgemm:\s*([\d.]+) sec', content)
-    reduce_scatter_z_time = re.search(r'Time to scatter and reduce Z:\s*([\d.]+) sec', content)
+    dgemm1_time = re.search(r'Time for first dgemm:\s*([\d.]+) sec', content)
+    dgemm2_time = re.search(r'Time for second dgemm:\s*([\d.]+) sec', content)
+    all2all_time = re.search(r'Time for all2all:\s*([\d.]+) sec', content)
+    reduce_scatter_time = re.search(r'Time for reduce-scatter:\s*([\d.]+) sec', content)
+    reduce_scatter_1_time = re.search(r'Time for first reduce-scatter:\s*([\d.]+) sec', content)
+    reduce_scatter_2_time = re.search(r'Time for second reduce-scatter:\s*([\d.]+) sec', content)
+    unpack_time = re.search(r'Time to unpack:\s*([\d.]+) sec', content)
+    pack_time = re.search(r'Time to pack for reduce-scatter:\s*([\d.]+) sec', content)
 
     return {
         'alg': alg,
         'impl': impl,
+        'system': system,
         'nnode': nnode,
         'nproc': nproc,
+        'nthread': nthread,
         'n': n,
-        'm': m,
         'r': r,
-        'matmul1p1': int(matmul1_grid.group(1)),
-        'matmul1p2': int(matmul1_grid.group(2)),
-        'matmul1p3': int(matmul1_grid.group(3)),
-        'matmul2p1': int(matmul2_grid.group(1)),
-        'matmul2p2': int(matmul2_grid.group(2)),
-        'matmul2p3': int(matmul2_grid.group(3)),
+        'matmul1p1': matmul1p1,
+        'matmul1p2': matmul1p2,
+        'matmul1p3': matmul1p3,
+        'matmul2p1': matmul2p1,
+        'matmul2p2': matmul2p2,
+        'matmul2p3': matmul2p3,
         'gen_omega_time': float(gen_omega_time.group(1)) if gen_omega_time else 0,
-        'first_dgemm_time': float(first_dgemm_time.group(1)) if first_dgemm_time else 0,
-        'redistrib_y_time': float(redistrib_y_time.group(1)) if redistrib_y_time else 0,
-        'second_dgemm_time': float(second_dgemm_time.group(1)) if second_dgemm_time else 0,
-        'reduce_scatter_z_time': float(reduce_scatter_z_time.group(1)) if reduce_scatter_z_time else 0,
+        'dgemm1_time': float(dgemm1_time.group(1)) if dgemm1_time else 0,
+        'dgemm2_time': float(dgemm2_time.group(1)) if dgemm2_time else 0,
+        'all2all_time': float(all2all_time.group(1)) if all2all_time else 0,
+        'reduce_scatter_time': float(reduce_scatter_time.group(1)) if reduce_scatter_time else 0,
+        'reduce_scatter_1_time': float(reduce_scatter_1_time.group(1)) if reduce_scatter_1_time else 0,
+        'reduce_scatter_2_time': float(reduce_scatter_2_time.group(1)) if reduce_scatter_2_time else 0,
     }
 
 def collect_experiment_data(directory):
